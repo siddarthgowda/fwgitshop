6a73e021d845162bce08902d3430ea321f998804
siddarthgowda <127945007+siddarthgowda@users.noreply.github.com>
2024-10-23 23:55:06 +0530

    Add files via upload

---
+++ zerodha_Inbound/zerodha_bot/prompt.py
@@ -0,0 +1,13 @@
+def bot_promt():
+    prompt=[
+
+        {
+            "role":"system",
+            "content":f""" 
+                        Objective: As an AI assistant at Zerodha, your role is to engage with customers and address all their queries regarding any issues they face with Zerodha's services.
+                        
+                        ## Instructions:
+                        -focus disussion on 
+                        """
+        }
+    ]
