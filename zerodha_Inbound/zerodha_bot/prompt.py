7aee732fc28ac14dac160eb947a9b0ddb8f03a3b
siddarthgowda <127945007+siddarthgowda@users.noreply.github.com>
2024-10-24 10:54:38 +0530

    Update prompt.py

--- zerodha_Inbound/zerodha_bot/prompt.py
+++ zerodha_Inbound/zerodha_bot/prompt.py
@@ -7,7 +7,7 @@ def bot_promt():
                         Objective: As an AI assistant at Zerodha, your role is to engage with customers and address all their queries regarding any issues they face with Zerodha's services.
                         
                         ## Instructions:
-                        -focus disussion on 
+                        
                         """
         }
     ]
